e8a765586ac82a98f825d0f88416acf45731cc98
mariobenissimo <mariobenissimo@gmail.com>
2023-05-23 23:10:55 +0200

    feat: "change name of handlers"

--- cmd/main.go
+++ cmd/main.go
@@ -13,9 +13,12 @@ func main() {
 	db.InizializeDatabase()
 	// inizializeService()
 	r := mux.NewRouter()
-	r.HandleFunc("/getMovies", handlers.GetMovies).Methods("GET")
+	r.HandleFunc("/movies", handlers.GetMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", handlers.GetMoviesId).Methods("GET")
 	r.HandleFunc("/movies", handlers.Create).Methods("POST")
+
+	//PUT /api/users/{id}: Updates the details of an existing user based on the ID.
+	//DELETE /api/users/{id}: Deletes a user from the database based on the ID.
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
